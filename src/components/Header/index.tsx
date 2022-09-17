7b95c0df088d427cc849eee16df367e0da690278
Bruno Seleghin <brunoseleghin@gmail.com>
2022-09-17 20:43:52 -0300

    :sparkles: feat: Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/Logo.svg';
+import styles from './styles.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt='Logo todo' />
+    </header>
+  );
+}
