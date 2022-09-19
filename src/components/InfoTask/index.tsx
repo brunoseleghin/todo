d48ff8e1bbd9bace2936563021575990d49332d6
Bruno Seleghin <brunoseleghin@gmail.com>
2022-09-18 22:20:02 -0300

    :sparkles: feat: Added InfoTask component

---
+++ src/components/InfoTask/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.css';
+
+export function InfoTask() {
+  return (
+    <div className={styles.wrapperInfo}>
+      <div className={styles.wrapperTasks}>
+        <p className={styles.createdTasks}>Tarefas criadas</p>
+        <p className={styles.countTask}>0</p>
+      </div>
+
+      <div className={styles.wrapperTasks}>
+        <p className={styles.completedTasks}>Concluídas</p>
+        <p className={styles.countTask}>0</p>
+      </div>
+    </div>
+  );
+}
