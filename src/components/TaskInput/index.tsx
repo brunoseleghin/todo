d53ec49736369a13442e637b9811551a47c2437a
Bruno Seleghin <brunoseleghin@gmail.com>
2022-09-20 22:57:18 -0300

    :sparkles: feat: Added functionality for input

--- src/components/TaskInput/index.tsx
+++ src/components/TaskInput/index.tsx
@@ -1,15 +1,45 @@
 import { PlusCircle } from 'phosphor-react';
-import styles from './styles.module.css'
+import { ChangeEvent, useState } from 'react';
+import { TaskProps } from '../../App';
+import styles from './styles.module.css';
 
-export function TaskInput() {
+interface TaskInputProps {
+  addTask: (task: string) => void;
+}
+
+export function TaskInput({ addTask }: TaskInputProps) {
+  const [task, setTask] = useState('');
+
+  function handleAddNewTask(event: ChangeEvent<HTMLInputElement>) {
+    const newTask = event.target.value;
+
+    if (!newTask)
+      return;
+
+    setTask(newTask);
+  }
+
+  function handleAddTasks() {
+    if (!task)
+      return
+
+    addTask(task);
+    setTask('');
+  }
+
   return (
     <div className={styles.wrapper}>
       <input
         className={styles.input}
+        onChange={(value) => handleAddNewTask(value)}
+        value={task}
         placeholder='Adicione uma nova tarefa'
       />
 
-      <button className={styles.button}>
+      <button
+        className={styles.button}
+        onClick={handleAddTasks}
+      >
         Criar <PlusCircle size={23} />
       </button>
     </div>
