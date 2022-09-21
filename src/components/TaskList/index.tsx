c7f822af17664bb2ecdbd60e4aa7a4c39a811644
Bruno Seleghin <brunoseleghin@gmail.com>
2022-09-20 22:55:46 -0300

    :sparkles: feat: Added TaskList component

---
+++ src/components/TaskList/index.tsx
@@ -0,0 +1,40 @@
+import styles from './styles.module.css';
+
+
+
+import Clipboard from '../../assets/Clipboard.svg'
+import { TaskProps } from '../../App';
+import { TaskItem } from '../TaskItem';
+
+interface TaskListProps {
+  tasks: TaskProps[];
+  taskDone: (taskId: number) => void;
+}
+
+export function TaskList({ tasks, taskDone }: TaskListProps) {
+  return (
+    <>
+      {tasks.length > 0 ? (
+        <div>
+          {tasks.map(task => {
+            return (
+              <TaskItem key={task.id} task={task} taskDone={taskDone} />
+            )
+          })}
+        </div>
+      ) : (
+        <div className={styles.wrapperListEmpty}>
+          <div className={styles.wrapperImg}>
+            <img src={Clipboard} />
+          </div>
+
+          <div className={styles.wrapperText}>
+            <b>Você ainda não tem tarefas cadastradas</b>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        </div>
+      )
+      }
+    </>
+  )
+}
