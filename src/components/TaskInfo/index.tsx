96f3408abf1ed1c3c4af0315639710bfa753ec3a
Bruno Seleghin <brunoseleghin@gmail.com>
2022-09-20 22:58:44 -0300

    :recycle: refactor: Added task data

--- src/components/TaskInfo/index.tsx
+++ src/components/TaskInfo/index.tsx
@@ -1,16 +1,23 @@
+import { useEffect, useState } from 'react';
+import { TaskProps } from '../../App';
 import styles from './styles.module.css';
 
-export function TaskInfo() {
+interface TaskInfoProps {
+  tasks: TaskProps[];
+}
+
+export function TaskInfo({ tasks }: TaskInfoProps) {
   return (
     <div className={styles.wrapperInfo}>
       <div className={styles.wrapperTasks}>
         <p className={styles.createdTasks}>Tarefas criadas</p>
-        <p className={styles.countTask}>0</p>
+        <p className={styles.countTask}>{tasks.length}</p>
       </div>
 
       <div className={styles.wrapperTasks}>
         <p className={styles.completedTasks}>Concluídas</p>
-        <p className={styles.countTask}>0</p>
+        <p className={styles.countTask}>{0} de {tasks.length}
+        </p>
       </div>
     </div>
   );
